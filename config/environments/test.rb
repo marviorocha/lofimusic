2190e131beaef245919d85bfb509a01d613bc0eb
marviorocha <marviorocha@marviorocha.com>
2021-11-23 22:14:05 -0300

    Added email localhost test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -10,7 +10,7 @@ Rails.application.configure do
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
