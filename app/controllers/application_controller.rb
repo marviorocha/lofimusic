16e75616e7b0d4e96dd90df642bd9b5f8f600ef4
marviorocha <marviorocha@marviorocha.com>
2021-11-28 20:17:46 -0300

    Set params the user account devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_params_permitted, if: :devise_controller?
+
+  protected
+
+  def set_params_permitted
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username email role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username email role])
+  end
 end
